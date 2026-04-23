84b6d1ca83301aedc778ddc195d34ffa0e1f17c8
Tirth Patel <tirth2093@gmail.com>
2026-04-23 13:02:14 -0400

    test: remove unused assertThatThrownBy import from JwtUtilTest

--- src/test/java/com/authplatform/security/JwtUtilTest.java
+++ src/test/java/com/authplatform/security/JwtUtilTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.test.util.ReflectionTestUtils;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class JwtUtilTest {
 
